5517bee92ba37ae54d8256247a02777496330b87
ᴅ ɪ ɪ <aodirizky05@gmail.com>
2023-08-02 23:28:35 +0700

    .

--- app/home.js
+++ app/home.js
@@ -1,9 +1,18 @@
-import { View, Text } from "react-native";
+import { SafeAreaView, ScrollView, View, Text } from "react-native";
+import { Stack, useRouter } from "expo-router"
+
+// Custom Components & Styles
+import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from "../components"
+import { COLORS, icons, images, SIZES } from "../constants"
 
 export default function Home() {
     return (
-        <View>
-            <Text>Ini halaman utama</Text>
-        </View>
+        <SafeAreaView>
+            <ScrollView>
+                <View>
+                    <Text>This is home page</Text>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
     )
 }
